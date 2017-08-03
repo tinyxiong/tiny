2df2c1b8b160f6b74ab7df390b8a438a107c640f
xiongyibiao <799497060@qq.com>
2017-08-03 16:51:45 +0800

    maven项目构建， 加入swagger， spring boot

--- src/main/java/com/tinyxiong/Application.java
+++ src/main/java/com/tinyxiong/Application.java
@@ -1,4 +1,12 @@
 package com.tinyxiong;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
 }
